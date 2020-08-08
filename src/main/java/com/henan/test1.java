2ead93ec27cd84e93ab9adcd24c2c7c8373f902f
lidawu <lhnxxlstc@126.com>
2020-08-08 09:15:18 +0800

    测试

---
+++ src/main/java/com/henan/test1.java
@@ -0,0 +1,11 @@
+package com.henan;
+
+/**
+ * @author wu
+ * @create 2020-08-08 9:03
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
